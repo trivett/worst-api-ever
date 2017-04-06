bdd478239abcdbcc626aa33e466d5dfc68ddc6f0
Vincent Trivett <trivett@gmail.com>
2017-04-06 18:58:50 -0400

    no cheating!

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
   resources :users, only: [:index, :show], defaults: {format: :json} do
-    resources :messages, only: [:index, :show], defaults: {format: :json}
+    resources :messages, only: [:show], defaults: {format: :json}
   end
 
   root "welcome#index"
